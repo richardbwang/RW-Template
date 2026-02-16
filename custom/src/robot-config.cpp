7882b63b176c1609fbfedae1ae5bf88ef3d025fc
Octavio <krolexz@gmail.com>
2026-02-16 13:21:45 -0500

    fix(conductor): Apply review suggestions for track 'Configuring project with robot specifications'

--- custom/src/robot-config.cpp
+++ custom/src/robot-config.cpp
@@ -57,6 +57,9 @@ double distance_between_wheels = 12.3;
 double wheel_distance_in = (36.0 / 48.0) * 3.17 * M_PI;
 
 // PID Constants for movement
+// distance_* : Linear PID for straight driving
+// turn_*     : PID for turning in place
+// heading_correction_* : PID for heading correction during linear movement
 double distance_kp = 1.1, distance_ki = 0.1, distance_kd = 7;
 double turn_kp = 0.3, turn_ki = 0, turn_kd = 2.5;
 double heading_correction_kp = 0.6, heading_correction_ki = 0, heading_correction_kd = 4;
@@ -66,13 +69,24 @@ bool using_horizontal_tracker = true;
 bool using_vertical_tracker = true;   
 
 // Odom offsets (inches) from AGENT.md
+// These comments are in the perspective of a top down view of the robot when the robot is facing vertical      
+// Vertical distance from the center of the bot to the horizontal tracking wheel (in inches, positive is when the wheel is behind the center)
 double horizontal_tracker_dist_from_center = 2.5;
+// Horizontal distance from the center of the bot to the vertical tracking wheel (in inches, positive is when the wheel is to the right of the center)
 double vertical_tracker_dist_from_center = 1.0;
 
 double horizontal_tracker_diameter = 2.0; // Defaulting to 2.0 unless specified
 double vertical_tracker_diameter = 2.0;   // Defaulting to 2.0 unless specified
 
 // Distance Reset setup
+// Set all of these values to the distance from the respective distance sensor to the robot's center along the axis it faces(in inches)
+// The front sensor offset is the distance from the front distance sensor to the robot center along the Y axis  
+// The back sensor offset is the distance from the back distance sensor to the robot center along the Y axis    
+// The left sensor offset is the distance from the left distance sensor to the robot center along the X axis    
+// The right sensor offset is the distance from the right distance sensor to the robot center along the X axis  
+// All values should be positive numbers
+// If you are not using all four distance sensors, just set the unused ones to 0
+// If you are not using distance resets these values will be ignored
 double front_sensor_offset = 0.0;
 double left_sensor_offset = 0.0;
 double right_sensor_offset = 0.0;
